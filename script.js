5e93be1eb2f62f52a72e0019c0a40348c2fa9352
Guilherme Castro <guii_1@hotmail.com>
2022-07-05 11:40:44 -0300

    adicionado mecanica de swapTurn, para cada jogador ter sua vez de jogada.

---
+++ script.js
@@ -0,0 +1,35 @@
+let memoria = null
+let itYourTurn = true
+
+const swapTurn = () => {
+    itYourTurn = !itYourTurn
+}
+
+const positions = document.querySelectorAll("[data-cell]");
+        const movePiece = (e) => {
+        const piece = e.target.classList
+        const pieceToMove = itYourTurn ? "circulocreme" : "circulobrown" 
+        console.log(itYourTurn) 
+        console.log(piece.value)  
+        if (pieceToMove == piece.value && memoria == null) {
+            if (piece.length != 0 && memoria == null) {
+                memoria = piece.value
+                piece.remove(memoria)
+            } else {
+                if (piece.length == 0 && memoria != null) {
+                    piece.add(memoria)
+                    memoria = null
+                } else {
+                }
+            } } else if (piece.length == 0 && memoria != null) {
+                piece.add(memoria)
+                memoria = null
+                swapTurn()
+            } else {
+            }
+        };
+
+
+    for (const position of positions) {  
+        position.addEventListener('click', movePiece)
+    }
