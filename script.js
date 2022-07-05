64834a63bd8b1afee67c034cd1dad8aa07d66d0b
Guilherme Castro <guii_1@hotmail.com>
2022-07-05 13:19:28 -0300

    feito algumas alterações na estilização.

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
 let memoria = null
 let itYourTurn = true
-
+let player = ""
 const swapTurn = () => {
     itYourTurn = !itYourTurn
 }
+const sideMenu = document.querySelector(".telamenu")
+sideMenu.innerHTML = "Bem vindo ao jogo, ainda faltam algumas implementações."
+const p = document.createElement('p')
+sideMenu.append(p)
+
 
 const positions = document.querySelectorAll("[data-cell]");
         const movePiece = (e) => {
+        if (itYourTurn) {
+            player = "jogador das peças claras"
+        } else { player = "jogador das peças escuras"}
         const piece = e.target.classList
         const pieceToMove = itYourTurn ? "circulocreme" : "circulobrown" 
         console.log(itYourTurn) 
